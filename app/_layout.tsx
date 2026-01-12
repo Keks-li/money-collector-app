2ea19d3e62cf451f1747cf8974ce217ec0aa41db
Keks-Li <eforichbiz@gmail.com>
2026-01-12 15:15:34 +0000

    remove dashboard route since I don,t have any file/folder as such

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,8 @@ export default function RootLayout() {
     <DataProvider>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
-        <Stack.Screen name="dashboard" />
+       <Stack.Screen name="admin" />
+        <Stack.Screen name="agent" />
       </Stack>
     </DataProvider>
   );
